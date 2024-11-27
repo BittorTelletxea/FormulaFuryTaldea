322e03e4635d6ddb4544daafcc95775102e7fb1f
WG32 TALDEA <ik012982i5@H012982.NET>
2024-11-27 09:41:13 +0100

    Header

--- ForumulaFury/src/components/Footer.jsx
+++ ForumulaFury/src/components/Footer.jsx
@@ -6,29 +6,33 @@ function Footer() {
       <div className='container text-center '>
         <div className='argazkiak'>
         <div className='ligak'>
-          <div>
+          <div className='img'>
           <img src="../../public/images/ligakSelected.png.png"  />
           </div>
           <p>Ligak</p>
         </div>
         <div className='klasifikazioa'>
-          <div>
+          <div className='img'>
           <img src="../../public/images/klasifikazioa.png.png"  />
           </div>
           <p>Klasifikazioa</p>
         </div>
         <div className='taldea'>
-          <div>
+          <div className='img'>
           <img src="../../public/images/taldea.png.png"  />
           </div>
           <p>Taldea</p>
         </div>
         <div className='merkatua'>
+          <div className='img'>
           <img src="../../public/images/merkatua.png.png"  />
+          </div>
           <p>Merkatua</p>
         </div>
         <div className='aktibitatea'>
+          <div className='img'>
           <img src="../../public/images/aktibitatea.png"  />
+          </div>
           <p>Aktibitatea</p>
         </div>
         </div>
--- ForumulaFury/src/components/Header.jsx
+++ ForumulaFury/src/components/Header.jsx
@@ -4,6 +4,9 @@ function Hasiera() {
   return (
     <>
       <div className='container text-center '>
+        <div id='logo'>
+            <img src="../../public/images/header.png" alt="" />
+        </div>
       </div>
     </>
   );
