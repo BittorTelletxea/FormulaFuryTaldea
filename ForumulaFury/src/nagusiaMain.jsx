10d3703d2c1d2140f366a270642b167470c35660
WG32 TALDEA <ik012982i5@H012982.NET>
2024-11-27 10:36:03 +0100

    Hurrenngo

--- ForumulaFury/src/nagusiaMain.jsx
+++ ForumulaFury/src/nagusiaMain.jsx
@@ -3,12 +3,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
-import Nagusia from './components/Nagusia.jsx';
+import Lasterketa from './components/Lasterketa.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <Nagusia/>
+    <Lasterketa/>
     <Footer />
   </StrictMode>,
 )
