0dafcb345df2f985e663d56fbb8c83706abae516
WG32 TALDEA <ik012982i5@H012982.NET>
2024-11-27 09:10:26 +0100

    Footer bukatua

---
+++ ForumulaFury/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../footer.css';
+
+function Hasiera() {
+  return (
+    <>
+      <div className='container text-center '>
+      </div>
+    </>
+  );
+}
+
+export default Hasiera;
--- ForumulaFury/src/nagusiaMain.jsx
+++ ForumulaFury/src/nagusiaMain.jsx
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
     <Footer />
   </StrictMode>,
 )
